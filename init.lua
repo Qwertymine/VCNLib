99442ed914322ecd541321394131dcafcd1e9a97
qwertymine3 <qwertymine3@googlemail.com>
2015-08-24 00:26:33 +0100

    More work towards the basic api

--- init.lua
+++ init.lua
@@ -1,13 +1,82 @@
 local yaba = {}
 
+--Layer def
+--	name
+--		string
+--	dimensions
+--		2 or 3
+--	sector_lengths
+--		vector - norm 300^3 or 2000^3
+--	biome_types
+--		string - random,heatmap,heatmap_tol
+--	biome_type_options
+--		table - tollerances for heatmap
+--	geometery
+--		string - cartesian,taxicab,chess
+--
+--Layer in mem
+--	cache
+--		table of tables
+--	biome number
+--		number of biomes
+yaba:get_node_biome = function(pos,seed,layer)
+	local sector = yaba:pos_to_sector(pos)
+	local points = {}
+	if dims ==  3 then
+	for x=-1,1 do
+		for y=-1,1 do
+			for z=-1,1 do
+				local temp = yaba:generate_biomed_points(vactor.add(sector,{x=x,y=y,z=z}),seed,layer)
+				for i,v in ipairs(temp) do
+					table.insert(points,v)
+				end
+			end
+		end
+	end
+	else
+	for x=-1,1 do
+		for z=-1,1 do
+			local temp = yaba:generate_biomed_points(vactor.add(sector,{x=x,y=0,z=z}),seed,layer)
+			for i,v in ipairs(temp) do
+				table.insert(points,v)
+			end
+		end
+	end
+	end
+	local geo = layer.geometry
+	return find_closest(pos,geo,points)
+end
+	
+
+
+local get_biome_num = function(layer)
+	return table.getn(layer.biomes)
+end
+
 yaba:generate_biomed_points = function(sector,seed,layer)
-	local dims = layer.dimensions
 	local hash = minetest.hash_node_position(sector)
 	if layer.cache[hash] then
 		return layer.cache[hash]
 	end
 	local points,prand = yaba:generate_points(sector,seed,layer)
-	layer.cache[hash] = points
+	local biome_meth = layer.biome_types
+	local ret = {}
+	if biome_meth = "random" then
+		if not layer.biome_number then
+			layer.biome_number = get_biome_num(layer)
+		end
+		for i,v in ipairs(points) do
+			local num = prand:next(1,layer.biome_number)
+			table.insert({
+				pos = v
+				biome = layer.biomes[num]
+			},ret)
+		end
+	else
+	end
+	layer.cache[hash] = ret 
+	return ret
+end
 
 yaba:generate_points = function(sector,seed,layer)
 	local hash = minetest.hash_node_position(sector)
@@ -15,26 +84,38 @@ yaba:generate_points = function(sector,seed,layer)
 	local lim = 2
 	local num = prand:next(1,20)
 	local points = {}
+	local dims = layer.dimensions
 	if num < 20 then
 		num = 1
 	else
 		num = 2
 	end
-	while num > 0 do
-		local x = prand:next(0,self[layer].sector_lengths.x-1)
-		local y = prand:next(0,self[layer].sector_lengths.y-1)
-		local z = prand:next(0,self[layer].sector_lengths.z-1)
-		local pos = {x=x,y=y,z=z}
-		pos = vector.add(pos,yaba:sector_to_pos_3d(sector,layer))
-		table.insert(points,pos)
-		num = num - 1
+	if dims == 3 then
+		while num > 0 do
+			local x = prand:next(0,self[layer].sector_lengths.x-1)
+			local y = prand:next(0,self[layer].sector_lengths.y-1)
+			local z = prand:next(0,self[layer].sector_lengths.z-1)
+			local pos = {x=x,y=y,z=z}
+			pos = vector.add(pos,yaba:sector_to_pos_3d(sector,layer))
+			table.insert(points,pos)
+			num = num - 1
+		end
+	else
+		while num > 0 do
+			local x = prand:next(0,self[layer].sector_lengths.x-1)
+			local y = 0
+			local z = prand:next(0,self[layer].sector_lengths.z-1)
+			local pos = {x=x,y=y,z=z}
+			pos = vector.add(pos,yaba:sector_to_pos_3d(sector,layer))
+			table.insert(points,pos)
+			num = num - 1
+		end
 	end
 	return points , prand
-	end
 end
 
 yaba:sector_to_pos = function(sector,layer)
-	local lengths = self[layer].sector_lengths
+	local lengths = layer.sector_lengths
 	local pos = {}
 	local dims = layer.dimensions
 	if dims == 3 then
@@ -45,11 +126,12 @@ yaba:sector_to_pos = function(sector,layer)
 		pos.x = lengths.x * sector.x
 		pos.y = 0
 		pos.z = lengths.z * sector.z
+	end
 	return pos
 end
 
 yaba:pos_to_sector = function(pos,layer)
-	local lengths = self[layer].sector_lengths
+	local lengths = layer.sector_lengths
 	local dims = layer.dimensions
 	local sector = {pos.x,pos.y,pos.z}
 	if dims == 3 then
