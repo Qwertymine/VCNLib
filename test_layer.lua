c7895b98044e72c2d22d1e5a715f1e671483f6b2
qwertymine3 <qwertymine3@googlemail.com>
2015-08-30 11:11:55 +0100

    Documentation

--- test_layer.lua
+++ test_layer.lua
@@ -1,11 +1,21 @@
 yaba.new_layer{
+	--name of the layer, used to get a copy of it later
 	name = "test",
+	--number of dimensions the noise changes over
 	dimensions = 2,
+	--scale to multiply the noise by(for performace)
 	scale = nil,
+	--side lengths for sectors (approx size for one biome)
 	sector_lengths = {
 	x=10,y=0,z=10,},
+	--how biomes are chosen
 	biome_types = "random",
+	--biome distribution options (if any)
+	random = nil,
+	--how distance from the centre of a biome is judged
+	--changes he shape of generated biomes
 	geometry = "manhattan",
+	--debug biomes, do not add biomes in this way - use layer:addbiome{def}
 	biomes = {
 		"bland",
 		"boring",
